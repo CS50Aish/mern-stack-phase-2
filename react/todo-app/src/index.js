36db47833b8912a2b9e07665533e16b7e0ee9013
aishwarya <aishwaryawaghmare15@gmail.com>
2024-03-03 16:11:13 +0000

    Updated the files

--- react/todo-app/src/index.js
+++ react/todo-app/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import useFetch from './userFetch';
+
+const Home = () => {
+  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
+
+  return{
+
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
