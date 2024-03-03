a10631a41d4ddadc78848c6cbcdfd24d0cb66a13
aishwarya <aishwaryawaghmare15@gmail.com>
2024-03-03 17:12:51 +0000

    Added Router Client

--- react/router-client/src/App.js
+++ react/router-client/src/App.js
@@ -4,5 +4,19 @@ import Home from "./components/Home";
 import About from "./components/About";
 
 function App(){
-  
+  return (
+    <div>
+      <h1>MERN APP</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
