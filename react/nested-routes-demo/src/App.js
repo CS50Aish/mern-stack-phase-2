b17bec0a2834ef03285fee29f63a355bcdf8ddf4
Ubuntu <labuser@ubuntu2204.n4cfe5p5jj4ejkqnx1luh20pjd.rx.internal.cloudapp.net>
2024-03-10 14:29:03 +0000

    Dynamic Pages

---
+++ react/nested-routes-demo/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Routes, Route, Link } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Footer from './components/Footer';
+
+function App(){
+  return (
+    <div>
+      <Header />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
