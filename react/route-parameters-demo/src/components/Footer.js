f5d68ea505c0970d04883d8cfaf27e6f199c2789
Ubuntu <labuser@ubuntu2204.n4cfe5p5jj4ejkqnx1luh20pjd.rx.internal.cloudapp.net>
2024-03-10 14:35:05 +0000

    Dynamic Pages

---
+++ react/route-parameters-demo/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(){
+    return (
+        <footer>
+            <p>@ Routes Parameter Demo</p>
+        </footer>
+    );
+}
+
+export default Footer;
--- react/route-parameters-demo/src/components/Header.js
+++ react/route-parameters-demo/src/components/Header.js
@@ -1,2 +1,22 @@
 import React from 'react';
-import Link from 
+import Link from 'react-router-dom';
+
+function Header(){
+    return (
+        <header>
+            <h1>Route Parameters Demo</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/user/123">User Profile</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
