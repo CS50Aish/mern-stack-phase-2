cda26eaa44d20d8aa8acaa50b3891fd35dbaa06a
aishwarya <aishwaryawaghmare15@gmail.com>
2024-03-10 17:14:34 +0000

    Dynamic Pages

--- react/protected-routes-demo/src/App.js
+++ react/protected-routes-demo/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import MenuBar from './components/MenuBar';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import Products from './components/Products';
+import Protected from './components/Protected';
 
-function App() {
+export default function App(){
+  const[isSignedIn, setIsSignedIn] = useState(null);
+  const signIn = () => {
+    setIsSignedIn(true);
+  }
+
+  const signOut = () => {
+    setIsSignedIn(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-2'>
+      <h2 className='mb-5 text-center'>React Protected Routes Example</h2>
+      <MenuBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path='/dashboard' element={<Protected isSignedIn={isSignedIn}><Dashboard /></Protected>}></Route>
+        <Route path='/products' element={<Protected isSignedIn={isSignedIn}><Products /></Protected>}></Route>
+      </Routes>
+      {isSignedIn ? (
+        <div className='d-grid mt-2'>
+          <button className='btn-danger' onClick={signOut}>Sign Out</button>
+        </div>
+      ) : (
+        <div className='d-grid mt-2'>
+          <button className='btn-dark' onClick={signIn}>Sign In</button>
+        </div>
+      )}
     </div>
-  );
+  )
 }
-
-export default App;
