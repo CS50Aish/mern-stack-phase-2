381c08c80d6463b0c783de3c4dadac9ab7f7b2fd
Ubuntu <labuser@ubuntu2204.tbmx5jx0jununil3ilzws0fexd.rx.internal.cloudapp.net>
2024-03-24 14:37:40 +0000

    Banking Web Application

---
+++ banking-app/my-banking-app-frontend/src/components/indexPage.js
@@ -0,0 +1,7 @@
+import AOS from 'aos';
+import '../App.css';
+import '../style.css';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import 
